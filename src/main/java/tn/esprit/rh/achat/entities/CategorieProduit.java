d2d135f9670f3f1bb9184f118e6444c4ff6bea79
Youssef Mechichi <youssef.mechichi@esprit.tn>
2022-10-19 00:20:52 +0100

    changement categorieproduit

--- src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -26,7 +26,7 @@ public class CategorieProduit implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer idCategorieProduit;
+	private Long idCategorieProduit;
 	private String codeCategorie;
 	private String libelleCategorie;
 	@OneToMany(mappedBy = "categorieProduit")
